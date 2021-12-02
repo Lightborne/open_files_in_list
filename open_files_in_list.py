7e13ba80dd8438e670c6abeff530fc2677363303
victor <victor@elalemanyo.de>
2021-12-02 11:05:14 +0100

    Accept relative paths

--- open_files_in_list.py
+++ open_files_in_list.py
@@ -2,6 +2,7 @@ import sublime
 import sublime_plugin
 from os.path import isfile
 from os.path import abspath
+import re
 
 class OpenFilesInListCommand(sublime_plugin.WindowCommand):
     def run(self):
@@ -19,14 +20,24 @@ class OpenFilesInListCommand(sublime_plugin.WindowCommand):
             if (region.size() == 0):
                 continue
 
-            # If the file doesn't exist, keep the region around to highlight later
-            path = abspath(file_list_view.substr(region))
-            if (not isfile(path)):
-                not_found.append(region)
-                continue
+            rel_path = file_list_view.substr(region)
+            abs_path = os.path.abspath(rel_path)
+
+            # If the file doesn't exist, try to find inside open folders
+            if (not isfile(abs_path)):
+                for folder in self.window.folders():
+                    rel_path = re.sub(r"^.\/|^\/", "", rel_path)
+                    if (isfile(folder + "/" + rel_path)):
+                        abs_path = folder + "/" + rel_path
+                        continue
+
+                # If the file doesn't exist, keep the region around to highlight later
+                if (not isfile(abs_path)):
+                    not_found.append(region)
+                    continue
 
             # Should be able to open it now
-            self.window.open_file(path)
+            self.window.open_file(abs_path)
 
         # Highlight those not found
         file_list_view.add_regions("not_found", not_found, "invalid")
